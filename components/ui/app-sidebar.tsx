dbb79005ec345a5c32a464907f116bf7145a441c
Akash kumar kumawat <kumawatap63@gmail.com>
2024-11-18 23:44:11 +0530

    Added ui compoents

---
+++ components/ui/app-sidebar.tsx
@@ -0,0 +1,60 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+// Menu items.
+const items = [
+  {
+    title: "Create wallet",
+    url: "/",
+  },
+  {
+    title: "Connect wallet",
+    url: "/connect-wallet",
+  },
+  {
+    title: "Request Airdrop",
+    url: "/request-airdrop",
+  },
+  {
+    title: "Sign Message",
+    url: "/sign-message",
+  },
+  {
+    title: "Mint Token",
+    url: "/mint-token",
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant="floating">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
