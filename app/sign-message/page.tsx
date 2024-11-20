3c3eb253535ca8d0cbb14db128314318de1f8060
Akash kumar kumawat <kumawatap63@gmail.com>
2024-11-20 22:30:04 +0530

    Added sign-message

---
+++ app/sign-message/page.tsx
@@ -0,0 +1,9 @@
+import SignMessage from "@/components/SignMessage";
+
+export default function Home() {
+  return (
+    <div className="p-10 space-y-10">
+      <SignMessage />
+    </div>
+  );
+}
