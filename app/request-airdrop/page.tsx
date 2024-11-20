5af1e118755d991fbc643dacf7252aefae4ddf13
Akash kumar kumawat <kumawatap63@gmail.com>
2024-11-20 22:29:45 +0530

    Added request-airdrop

---
+++ app/request-airdrop/page.tsx
@@ -0,0 +1,9 @@
+import RequestAirdrop from "@/components/RequestAirdrop";
+
+export default function Home() {
+  return (
+    <div className="p-10 space-y-10">
+      <RequestAirdrop />
+    </div>
+  );
+}
